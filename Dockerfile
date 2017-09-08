5cc9142453ee08e01f34640ba6d7bf77c176d88d
josh bryans <joshhsoj1902@gmail.com>
2017-09-07 21:06:52 -0400

    Add Docker Runner File

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,14 @@ RUN git fetch --all && git reset --hard origin/master
 
 RUN git checkout tags/170710.1
 
+RUN find /home/steam/linuxgsm -type f -name "*.sh" -exec chmod u+x {} \; \
+ && find /home/steam/linuxgsm -type f -name "*.py" -exec chmod u+x {} \; \
+ && chmod u+x /home/steam/linuxgsm/lgsm/functions/README.md
+
 RUN  mkdir ~/bin \
   && curl -sSLf -z ~/bin/gomplate -o ~/bin/gomplate https://github.com/hairyhenderson/gomplate/releases/download/v2.0.0/gomplate_linux-amd64-slim \
   && chmod 755 ~/bin/gomplate
 
-#ADD docker-runner.sh ./
+ADD docker-runner.sh ./
 
-#CMD ["bash", "./docker-runner.sh"]
+CMD ["bash", "./docker-runner.sh"]
---
+++ docker-runner.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+"${LGSM_GAMESERVERNAME:?Need to set LGSM_GAMESERVERNAME environment }"
+if [ -z "$LGSM_GAMESERVERNAME" ]; then
+  echo "Need to set LGSM_GAMESERVERNAME environment"
+  exit 1
+fi
+
+if [ -n "$LGSM_UPDATEINSTALLSKIP" ]; then
+  case "$LGSM_UPDATEINSTALLSKIP" in
+  "UPDATE")
+      ./linuxgsm.sh $LGSM_GAMESERVERNAME
+      mv $LGSM_GAMESERVERNAME lgsm-gameserver
+      ./lgsm-gameserver auto-install
+      echo "Game has been updaed. Starting"
+      ;;
+  "INSTALL")
+      ./linuxgsm.sh $LGSM_GAMESERVERNAME
+      mv $LGSM_GAMESERVERNAME lgsm-gameserver
+      ls -ltr
+      ./lgsm-gameserver auto-install
+      echo "Game has been installed. Exiting"
+      exit
+      ;;
+  esac
+fi
+
+
+
+if [ ! -f lgsm-gameserver ]; then
+    echo "No game is installed, please set LGSM_UPDATEINSTALLSKIP"
+    exit 1
+fi
+
+~/bin/gomplate -f ~/linuxgsm/lgsm/config-default/config-lgsm/common.cfg.tmpl -o ~/linuxgsm/lgsm/config-lgsm/$LGSM_GAMESERVERNAME/common.cfg
+~/bin/gomplate -f ~/linuxgsm/lgsm/config-lgsm/$LGSM_GAMESERVERNAME/$LGSM_GAMESERVERNAME.cfg.tmpl -o ~/linuxgsm/lgsm/config-lgsm/$LGSM_GAMESERVERNAME/$LGSM_GAMESERVERNAME.cfg
+#
+./lgsm-gameserver start
+sleep 30s
+#
+./lgsm-gameserver details
+#
+while :
+do
+bash lgsm-gameserver monitor
+sleep 30s
+done
