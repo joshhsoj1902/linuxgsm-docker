60c7a7243cb03f43573185933fd16084d8d028fa
josh <joshhsoj1902@gmail.com>
2017-11-07 18:32:35 -0500

    When installing a game, report as healthy

--- docker-health.sh
+++ docker-health.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -f 'INSTALLING.LOCK' ]; then
+   echo "INSTALLING.LOCK found - PASS"
+   exit 0
+fi
+
 monitorstring=$(./lgsm-gameserver monitor)
 echo $monitorstring
 failcount=$(echo "$monitorstring" | grep -oh 'FAIL\|ERROR' | wc -w)
