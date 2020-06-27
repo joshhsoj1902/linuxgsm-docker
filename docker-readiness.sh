c5474c5e3a43e43c183c8a5df2ca47eed28d44b7
Josh <joshhsoj1902@gmail.com>
2020-06-27 09:53:08 -0400

    debugging

--- docker-readiness.sh
+++ docker-readiness.sh
@@ -3,6 +3,11 @@
 if [ -f 'INSTALLING.LOCK' ]; then
    echo "Found INSTALLING.LOCK - still installing - FAIL"
    exit 1
+else
+   echo "Installing done, checking game status"
 fi
 
-./lgsm-gameserver monitor
+exec 5>&1
+monitor_output=$(./lgsm-gameserver monitor|tee /dev/fd/5)
+
+echo $monitor_output
